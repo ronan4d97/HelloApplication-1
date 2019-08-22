f7c3f247c71c7917ae45ab93cabf8fb2776dc993
Ronan-Ramdas <ronan4d97@gmail.com>
2019-08-22 12:24:18 +0530

    Ronan: Added cool things to App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java -- We are Awesome !!!!");
+        System.out.println("Hello Java -- We are Awesome !!!!Java is cool");
         System.out.println("I am enjoying the training...");
+        System.out.println("Hello this is GIT Training");
     }
 }
